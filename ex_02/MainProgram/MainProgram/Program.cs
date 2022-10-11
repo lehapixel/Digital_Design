5b06edbed85930508c91b480acc0fa5c0fc259ea
Alexey Katunin <33673310+lehapixel@users.noreply.github.com>
2022-10-12 00:42:25 +0300

    Adding home exercise №2

---
+++ ex_02/MainProgram/MainProgram/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace MainProgram
+{
+    class Program
+    {
+        static void Main()
+        {         
+            string pathToFile = "D:/Projects/C#/Digital_Design/ex_02/Resources/WarAndPeaceVolume1.txt";
+            string[] text = File.ReadAllLines(pathToFile);
+
+            var type = typeof(WordCounter.WordCounter).Assembly.GetTypes().First(x => x.Name == "WordCounter");
+            var method = type.GetMethod("Count", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Static);
+            var dictionary = method.Invoke(null, new object[] { text }) as Dictionary <string, int>;
+            using (StreamWriter file = new("wordCounter.txt"))
+            {
+                foreach (var row in dictionary.OrderByDescending(_ => _.Value))
+                {
+                    file.WriteLine($"{row.Key} {row.Value}");
+                }
+            }
+            Console.WriteLine("File has been created!");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ex_02/WordCounter/WordCounter/WordCounter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WordCounter
+{
+    public class WordCounter
+    {
+        static private Dictionary<string, int> Count(ref string[] text)
+        {
+            Dictionary<string, int> keyValuePairs = new Dictionary<string, int>(); ;
+            foreach (var line in text)
+            {
+                var words = line.ToLower().Split(new char[] { ' ', ',', '.', '!', '?', ':', ';', '-', '–', '+', '=', '"', '«', '»', '[', ']', '(', ')', '/', '\\', '|', '~', '@', '$', '%', '^', '&', '*', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0' }, StringSplitOptions.RemoveEmptyEntries);
+                foreach (var word in words)
+                {
+                    if (!keyValuePairs.ContainsKey(word))
+                    {
+                        keyValuePairs.Add(word, 1);
+                    }
+                    else
+                    {
+                        keyValuePairs[word]++;
+                    }
+                }
+            }
+            return keyValuePairs;
+        }
+    }
+}
