d0ea0aa715be9c8483be5e8347fabb419fe63ede
Alexey Katunin <33673310+lehapixel@users.noreply.github.com>
2022-10-13 16:25:31 +0300

    Adding home exercise №3

---
+++ ex_03/MainProgram/MainProgram/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+using System.Collections.Concurrent;
+using System.Diagnostics;
+
+namespace MainProgram
+{
+    class Program
+    {
+        static void Main()
+        {
+            string pathToFile = "D:/Projects/C#/Digital_Design/ex_03/Resources/WarAndPeaceVolume1.txt";
+            string[] text = File.ReadAllLines(pathToFile);
+
+            Stopwatch stopWatchCount = new Stopwatch();
+
+            var type = typeof(WordCounter.WordCounter).Assembly.GetTypes().First(x => x.Name == "WordCounter");
+
+            //Single Threaded Count
+            var method = type.GetMethod("Count", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Static);
+            Dictionary<string, int> dictionary = new Dictionary<string, int>();
+            stopWatchCount.Start();
+            dictionary = method.Invoke(null, new object[] { text }) as Dictionary<string, int>;
+            stopWatchCount.Stop();
+            using (StreamWriter file = new("wordCounter.txt"))
+            {
+                foreach (var row in dictionary.OrderByDescending(_ => _.Value))
+                {
+                    file.WriteLine($"{row.Key} {row.Value}");
+                }
+            }
+            long timeSpan = stopWatchCount.ElapsedMilliseconds;
+            Console.WriteLine("Count Method Run Time: " + timeSpan + " ms");
+            Console.WriteLine("File \"wordCounter.txt\" has been created! \n");
+
+            //Multithreaded Count
+            method = type.GetMethod("MultithreadedCount", System.Reflection.BindingFlags.Public | System.Reflection.BindingFlags.Static);
+            ConcurrentDictionary<string, int> concurrentDictionary = new ConcurrentDictionary<string, int>(); 
+            stopWatchCount.Restart();
+            concurrentDictionary = method.Invoke(null, new object[] { text }) as ConcurrentDictionary<string, int>;
+            stopWatchCount.Stop();
+            using (StreamWriter file = new("multithreadedWordCounter.txt"))
+            {
+                foreach (var row in concurrentDictionary.OrderByDescending(_ => _.Value))
+                {
+                    file.WriteLine($"{row.Key} {row.Value}");
+                }
+            }
+            timeSpan = stopWatchCount.ElapsedMilliseconds;
+            Console.WriteLine("MultithreadedCount Method Run Time: " + timeSpan + " ms");
+            Console.WriteLine("File \"multithreadedwordCounter.txt\" has been created!");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ex_03/WordCounter/WordCounter/WordCounter.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Concurrent;
+using System.Threading.Tasks;
+
+namespace WordCounter
+{
+    public class WordCounter
+    {
+        static private Dictionary<string, int> Count(string[] text)
+        {
+            Dictionary<string, int> keyValuePairs = new Dictionary<string, int>(); ;
+            foreach (var line in text)
+            {
+                var words = line.ToLower().Split(new char[] { ' ', ',', '.', '!', '?', ':', ';', '-', '–', '+', '=', '"', '«', '»', '[', ']', '(', ')', '/', '\\', '|', '~', '@', '$', '%', '^', '&', '*', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0' }, StringSplitOptions.RemoveEmptyEntries);
+                foreach (var word in words)
+                {
+                    if (!keyValuePairs.ContainsKey(word))
+                    {
+                        keyValuePairs.Add(word, 1);
+                    }
+                    else
+                    {
+                        keyValuePairs[word]++;
+                    }
+                }
+            }
+            return keyValuePairs;
+        }
+
+        static public ConcurrentDictionary<string, int> MultithreadedCount(string[] text)
+        {
+            ConcurrentDictionary<string, int> keyValuePairs = new ConcurrentDictionary<string, int>();
+
+            Parallel.ForEach(text, line =>
+            {
+                var words = line.ToLower().Split(new char[] { ' ', ',', '.', '!', '?', ':', ';', '-', '–', '+', '=', '"', '«', '»', '[', ']', '(', ')', '/', '\\', '|', '~', '@', '$', '%', '^', '&', '*', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0' }, StringSplitOptions.RemoveEmptyEntries);
+
+                Parallel.ForEach(words, word =>
+                {
+                    keyValuePairs.AddOrUpdate(word, 1, (key, oldValue) => oldValue + 1);
+                });
+            });
+            return keyValuePairs;
+        }
+    }
+}
