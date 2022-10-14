ab406aa87be1275feffecfedc9364aabef4dcc4c
Alexey Katunin <33673310+lehapixel@users.noreply.github.com>
2022-10-14 06:06:14 +0300

    Adding home exercise №2. Patch ver. 0.4

--- ex_02/WordCounter/WordCounter/WordCounter.cs
+++ ex_02/WordCounter/WordCounter/WordCounter.cs
@@ -6,6 +6,7 @@ namespace WordCounter
     public class WordCounter
     {
         static private char[] exceptionalSymbols = { ' ', ',', '.', '!', '?', ':', ';', '-', '–', '+', '=', '"', '«', '»', '[', ']', '(', ')', '/', '\\', '|', '~', '@', '$', '%', '^', '&', '*', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0' };
+        
         static private Dictionary<string, int> Count(ref string[] text)
         {
             Dictionary<string, int> dictionary = new Dictionary<string, int>();
